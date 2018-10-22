a48f56d5c09306e871e77ee9ecc72899947b5eff
J Patrick <jpatricknola@gmail.com>
2018-10-22 16:11:27 -0500

    button

--- server/index.js
+++ server/index.js
@@ -3,12 +3,9 @@ const path = require('path');
 const bodyParser = require('body-parser');
 // UNCOMMENT THE DATABASE YOU'D LIKE TO USE
 const works = require('../database-mysql');
-// const items = require('../database-mongo');
+
 
 const app = express();
-
-// UNCOMMENT FOR REACT
-// app.use(express.static(__dirname + '/../react-client/dist'));
 
 // UNCOMMENT FOR ANGULAR
 app.use(express.static(path.join(__dirname, '/../angular-client')));
